767d27e9fdc3d1795365e2a294b4a89645d050cc
Yuri-stack <yurioliveirasilva1999@gmail.com>
2025-02-07 13:27:53 -0300

    Código Atualizado

--- src/Exemplo3.ts
+++ src/Exemplo3.ts
@@ -8,5 +8,15 @@ function listarPessoa(nome: string, idade: number) {
 const pessoa: [string, number] = ['Márcia da Silva', 30]; // tupla
 // const x: ["Marcia", 30, true, 8.5]
 
-console.log(listarPessoa(pessoa[0], pessoa[1]));
+// console.log(listarPessoa(pessoa[0], pessoa[1]));
 console.log(listarPessoa(...pessoa));
+
+console.log(" ");
+
+let vetor: number[] = [0, 1, 2, 3];
+
+function listarDados(...num: number[]): void {  // Aqui usamos o Rest Operator
+    console.log('{', ...num, '}');              // Aqui usamos o Rest Operator
+}
+
+listarDados(...vetor);  // Aqui usamos o Spread Operator
